2fe282647100d8b3cf8e9f35b22cf236bec54952
Masanov <masanav.ag@gmail.com>
2021-02-20 18:31:07 +0300

    third

--- src/game.java
+++ src/game.java
@@ -6,13 +6,14 @@ public class game {
     public static void main(String[] args) {
 
         String text = "*-ки *должны* примыкать к *СЛОВУ*)";
-        Pattern pattern = Pattern.compile("\*[А-Яа-я]+\*");
+        Pattern pattern = Pattern.compile("...");
         Matcher matcher = pattern.matcher(text);
         while(matcher.find()){
             int start = matcher.start();
             int end = matcher.end();
             System.out.print(text.substring(start,end) + " ");
-            System.out.print(text.substring("end");
+            System.out.print("end");
+            System.out.print("new");
         }
 
 
